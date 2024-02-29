50f6c7751f1dce0ef367997e715e34f0b2423482
yacineahmaich <yacineahmaich@gmail.com>
2024-02-29 15:08:10 +0100

    style: add logos to our-clients section

--- components/home/clients.tsx
+++ components/home/clients.tsx
@@ -5,28 +5,34 @@ const Clients = () => {
 
   const clients = [
     {
-      img: "/images/testimonial-1.jpg",
+      name: "Ecowatt",
+      img: "/images/partners/ecowatt.webp",
+      url: "https://www.ecowatt.ma",
     },
     {
-      img: "/images/testimonial-1.jpg",
+      name: "Taqaunivers",
+      img: "/images/partners/taqaunivers.webp",
+      url: "https://www.taqaunievrs.ma",
     },
     {
-      img: "/images/testimonial-1.jpg",
+      name: "Mafatih Arahma",
+      img: "/images/partners/mafatih-rahma.webp",
     },
     {
-      img: "/images/testimonial-1.jpg",
+      name: "Ecotaqa",
+      img: "/images/partners/ecotaqa.webp",
+      url: "https://www.ecotaqa.ma",
     },
     {
-      img: "/images/testimonial-1.jpg",
-    },
-    {
-      img: "/images/testimonial-1.jpg",
+      name: "IES",
+      img: "/images/partners/ies.webp",
     },
   ];
 
   return (
     <section className="container font-oxanium">
       <div className="space-y-10">
+        {/* Heading */}
         <div className="space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-xl font-semibold text-primary-dark">
@@ -40,16 +46,20 @@ const Clients = () => {
             Here are a few projects we worked on recently for clients worldwide.
           </p>
         </div>
+
+        {/* Content */}
         <div ref={emblaRef} className="embla [--slide-size:20%]">
           <div className="embla__container">
             {clients.map((client, idx) => (
-              <div key={idx} className="embla__slide p-6">
-                <div className="mx-auto aspect-square h-32 rounded-full border-4 bg-gray-100 shadow-xl">
-                  <img
-                    src={client.img}
-                    className="rounded-full object-cover"
-                    alt={client.img}
-                  />
+              <div key={idx} className="embla__slide">
+                <div className="mx-auto flex aspect-square items-center">
+                  <a href={client.url} target="_blank">
+                    <img
+                      src={client.img}
+                      className="rounded-full object-cover"
+                      alt={client.name}
+                    />
+                  </a>
                 </div>
               </div>
             ))}
